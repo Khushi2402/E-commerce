48657c1f07003501279854ef85c1ad03ac805934
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-03-19 19:39:26 +0530

    Added basic navbar and  hero section to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home></Home>
     </div>
   );
 }
