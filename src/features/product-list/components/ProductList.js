98d445583f27e0c7219f3184788952ec84aa7296
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-03-20 21:31:52 +0530

    7 - Filter and Sort changes

--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -208,23 +208,34 @@ export default function ProductList() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const products = useSelector(selectAllProducts);
   const [filter, setFilter] = useState({});
+  const [sort, setSort] = useState({});
 
   const handleFilter = (e, section, option) => {
-    const newFilter = { ...filter, [section.id]: option.value };
+    console.log(e.target.checked)
+    const newFilter = {...filter};
+    if(e.target.checked) {
+      if(newFilter[section.id]){
+        newFilter[section.id].push(option.value)
+      }else{
+        newFilter[section.id]=[option.value]
+      }
+    }
+    else{
+      const index = newFilter[section.id].find(el=>el===option.value)
+      newFilter[section.id].splice(index,1)
+    }
+    console.log({newFilter});
     setFilter(newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
-    console.log(section.id, option.value);
   };
 
   const handleSort = (e, option) => {
-    const newFilter = { ...filter, _sort: option.sort };
-    setFilter(newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
+    const sort = { _sort: option.sort };
+    setSort(sort);
   };
 
   useEffect(() => {
-    dispatch(fetchAllProductsAsync());
-  }, [dispatch]);
+    dispatch(fetchProductsByFiltersAsync({filter, sort}));
+  }, [dispatch, filter]);
 
   return (
     <div>
