435718591828b6713d0c443325668ca9a935ccbb
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-03-20 21:00:05 +0530

    Component Division

--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -320,9 +320,8 @@ export default function ProductList() {
 
             {/* section of product and filter ends here */}
 
-            <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+            
               <Pagination></Pagination>
-            </div>
           </main>
         </div>
       </div>
@@ -507,6 +506,7 @@ function DesktopFilter({handleFilter}) {
 function Pagination() {
   return (
     <>
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
         <a
           href="#"
@@ -565,6 +565,7 @@ function Pagination() {
             </a>
           </nav>
         </div>
+      </div>
       </div>
     </>
   );
