c35304284b53d23b975f6652025ca801d804688f
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-03-20 15:29:44 +0530

    Demo API setup done

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -1,7 +1,6 @@
-
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:3030")
+    const response = await fetch("http://localhost:8080/products")
     const data = await response.json()
     resolve({data})
   }
